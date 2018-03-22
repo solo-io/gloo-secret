a2a1dc6d590c481eac1dd52f2127867c61e4dc50
Ashish Shrestha <axhixh@gmail.com>
2018-03-22 11:07:59 -0400

    match the way gloo expects file based secrets

--- file/file_client.go
+++ file/file_client.go
@@ -10,10 +10,6 @@ import (
 	secret "github.com/solo-io/gloo-secret"
 )
 
-const (
-	secretsFolder = "secrets"
-)
-
 func NewClient(dir string) (secret.SecretInterface, error) {
 	info, err := os.Stat(dir)
 	if err != nil {
@@ -22,16 +18,6 @@ func NewClient(dir string) (secret.SecretInterface, error) {
 
 	if !info.IsDir() {
 		return nil, errors.New(dir + " is not a directory")
-	}
-
-	dir = filepath.Join(dir, secretsFolder)
-	_, err = os.Stat(dir)
-	if err != nil {
-		if os.IsNotExist(err) {
-			os.Mkdir(dir, 0755)
-		} else {
-			return nil, errors.Wrap(err, "error setting up secrets folder")
-		}
 	}
 	v := V1{client: &v1Client{dir: dir}}
 	return &v, nil
@@ -58,33 +44,26 @@ func (c *v1Client) Create(s *secret.Secret) (*secret.Secret, error) {
 	if exists {
 		return nil, errors.Errorf("secret with name %s already exists", s.Name)
 	}
-	s.ResourceVersion = newOrIncrementResourceVer(s.ResourceVersion)
-	filename := filepath.Join(c.dir, s.Name+".json")
-	if err := WriteToFile(filename, SecretToFS(s)); err != nil {
+	p, d := SecretToFS(s)
+	filename := filepath.Join(c.dir, p)
+	if err := WriteToFile(filename, d); err != nil {
 		return nil, errors.Wrap(err, "unable to save secret")
 	}
 	return s, nil
 }
 
 func (c *v1Client) Update(s *secret.Secret) (*secret.Secret, error) {
-	if s.ResourceVersion == "" {
-		return nil, errors.New("updating secret requires resource version")
-	}
-
 	secrets, err := load(c.dir)
 	if err != nil {
 		return nil, errors.Wrap(err, "unable to load existing secrets")
 	}
-	existing, exists := secrets[s.Name]
+	_, exists := secrets[s.Name]
 	if !exists {
 		return nil, errors.Errorf("secret %s does not exist", s.Name)
 	}
-	if lessThan(s.ResourceVersion, existing.ResourceVersion) {
-		return nil, errors.New("resource version outdated")
-	}
-	s.ResourceVersion = newOrIncrementResourceVer(s.ResourceVersion)
-	filename := filepath.Join(c.dir, s.Name+".json")
-	if err := WriteToFile(filename, SecretToFS(s)); err != nil {
+	p, d := SecretToFS(s)
+	filename := filepath.Join(c.dir, p)
+	if err := WriteToFile(filename, d); err != nil {
 		return nil, errors.Wrap(err, "unable to save secret")
 	}
 	return s, nil
@@ -99,22 +78,21 @@ func (c *v1Client) Delete(name string) error {
 	if !exists {
 		return errors.Errorf("secret %s does not exist", name)
 	}
-	if err := os.Remove(filepath.Join(c.dir, name+".json")); err != nil {
+	if err := os.Remove(filepath.Join(c.dir, name)); err != nil {
 		return err
 	}
 	return nil
 }
 
 func (c *v1Client) Get(name string) (*secret.Secret, error) {
-	secrets, err := load(c.dir)
+	filename := filepath.Join(c.dir, name)
+	data := make(map[string]string)
+	err := ReadFileInto(filename, &data)
 	if err != nil {
-		return nil, errors.Wrap(err, "unable to load existing secrets")
+		return nil, errors.Wrapf(err, "unable to get secret %s", name)
 	}
-	s, exists := secrets[name]
-	if !exists {
-		return nil, errors.Errorf("secret %s does not exist", name)
-	}
-	return s, nil
+
+	return SecretFromFS(name, data), nil
 }
 
 func (c *v1Client) List() ([]*secret.Secret, error) {
@@ -139,16 +117,16 @@ func load(dir string) (map[string]*secret.Secret, error) {
 	secrets := make(map[string]*secret.Secret)
 	for _, f := range files {
 		name := f.Name()
-		if !strings.HasSuffix(name, ".json") {
+		if !strings.HasSuffix(name, ".yaml") && !strings.HasSuffix(name, ".yml") {
 			continue
 		}
 
-		var fs fileSecret
-		err := ReadFileInto(filepath.Join(dir, name), &fs)
+		data := make(map[string]string)
+		err := ReadFileInto(filepath.Join(dir, name), &data)
 		if err != nil {
 			return nil, errors.Wrapf(err, "unable to parse %s as secret", name)
 		}
-		s := SecretFromFS(&fs)
+		s := SecretFromFS(name, data)
 		secrets[s.Name] = s
 	}
 	return secrets, nil
